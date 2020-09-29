2571fd1c3529813b565e535e8b15e656e4984cdd
pupilman <542846492@qq.com>
2020-09-29 23:36:36 +0800

    flowable 独自封装

---
+++ flowable-system/src/main/java/com/scd/flowablesystem/FlowableSystemApplication.java
@@ -0,0 +1,15 @@
+package com.scd.flowablesystem;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowableSystemApplication.class, args);
+  }
+
+}
+
---
+++ flowable-system/src/test/java/com/scd/flowablesystem/FlowableSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.scd.flowablesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
